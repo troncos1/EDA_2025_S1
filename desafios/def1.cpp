eaa2b39a34bf3936d1f3af06bcc5ee32bde20808
troncos1 <jorgmoreno@alumnos.uai.cl>
2025-03-24 11:59:41 -0300

    h

---
+++ desafios/def1.cpp
@@ -0,0 +1,26 @@
+// hay que crear un código que agregue datos de un usuario 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+// Ejercicio aprenderaprogramar.com
+int main() {
+    int valorUsuario;
+    printf("Introduzca su peso en kg como valor numerico: ");
+    scanf("%s", &valorUsuario);
+    printf("Gracias");
+    return 0;
+}
+[{
+	"resource": "/c:/Users/jorge/Documents/codes/EDA/EDA_2025_S1/desafios/def1.cpp",
+	"owner": "C/C++: IntelliSense",
+	"code": "1696",
+	"severity": 8,
+	"message": "cannot open source file \"stdio.h\". Please run the 'Select IntelliSense Configuration...' command to locate your system headers.",
+	"source": "C/C++",
+	"startLineNumber": 4,
+	"startColumn": 1,
+	"endLineNumber": 4,
+	"endColumn": 19
+}]
---
+++ dockerfile
@@ -0,0 +1,21 @@
+FROM ubuntu:22.04
+
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+RUN apt-get install -y vim build-essential git cmake net-tools gdb clang
+RUN apt-get install nano -y
+
+# RUN apt-get install libreria
+
+WORKDIR /work 
+# COPY main.c . 
+# RUN gcc main.c -o main
+# RUN ./main 
+
+
+# comandos:
+# crear imagen a partir de un dockerfile
+# docker build -t testj1 . 
+# crear un container a partir de la imagen, con dirección específica 
+# docker run --name ubuntu -v "C:\Users\jorge\OneDrive\Documentos\codes\vscode_docker-1:/work" -it testj1
+# ejecutar el entorno se usa el nombre del container
+# docker exec -it ubuntu bash
